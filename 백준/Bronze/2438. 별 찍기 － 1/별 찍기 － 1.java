ec0df329331b33e80a0df1216e36b641ee214e20
youbbin <91596873+youbbin@users.noreply.github.com>
2023-07-17 10:22:32 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 224 ms, Memory: 14892 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
