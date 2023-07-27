9449a08ddbd472c8c772af862b4b1529df1c18c9
youbbin <91596873+youbbin@users.noreply.github.com>
2023-07-27 09:56:55 +0900

    [Bronze V] Title: 아스키 코드, Time: 124 ms, Memory: 14308 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println((int)(br.readLine().charAt(0)));
+        br.close();
+    }
+}
