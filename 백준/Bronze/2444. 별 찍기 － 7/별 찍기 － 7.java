d96a7746979907ce59af71b650c9be1d7734da4e
LEE YOUBIN <91596873+youbbin@users.noreply.github.com>
2023-08-14 16:57:33 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 536 ms, Memory: 20980 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            for (int j = N - i - 1; j > 0; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = N - 1; i >= 0; i--) {
+            for (int j = N - i; j > 0; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        br.close();
+    }
+}
