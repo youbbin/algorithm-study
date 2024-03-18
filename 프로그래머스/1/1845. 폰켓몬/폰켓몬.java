d3aea590de3f787900e5a1a48b54268900902649
LEE YOUBIN <youbbqlsl@gmail.com>
2024-03-18 17:13:56 +0900

    [level 1] Title: 폰켓몬, Time: 3.73 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+
+class Solution {
+    public int solution(int[] nums) {
+       int answer = 0;
+        int n = nums.length;
+        HashMap<Integer, Integer> hashmap = new HashMap<>();
+        for(int i = 0; i < n; i++){
+            if(hashmap.containsKey(nums[i])){
+                hashmap.put(nums[i], hashmap.get(nums[i] + 1));
+            }
+            else{
+                hashmap.put(nums[i], 1);
+            }
+        }
+
+        int type_num = hashmap.size();
+        if(type_num <= n/2){
+            answer = type_num;
+        }
+        else{
+            answer = n/2;
+        }
+       
+        return answer;
+    }
+}
