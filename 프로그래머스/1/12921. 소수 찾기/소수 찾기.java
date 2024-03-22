0039d992e99f697ec862a76fdcfff604392442df
LEE YOUBIN <youbbqlsl@gmail.com>
2024-03-22 19:36:15 +0900

    [level 1] Title: 소수 찾기, Time: 206.26 ms, Memory: 52 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        boolean flag = false;
+        for(int i = 2; i <= n; i++){
+            if(isPrime(i)) answer++;
+        }
+        return answer;
+    }
+    
+    public boolean isPrime(int num){
+        for(int i = 2; i <= Math.sqrt(num); i++){
+            if(num % i == 0) return false;
+        }
+        return true;
+    }
+}
