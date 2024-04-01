7453244edde91a79670cdfcc77b6bdc706c093f5
LEE YOUBIN <youbbqlsl@gmail.com>
2024-04-01 10:16:26 +0900

    [level 2] Title: H-Index, Time: 0.49 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,22 +1,37 @@
 import java.util.*;
 class Solution {
-     public int solution(int[] citations) {
-        int[] hIdxArr = new int[citations.length];
+    // 코드 1
+//      public int solution(int[] citations) {
+//         int[] hIdxArr = new int[citations.length];
+//         for(int i = 0; i < citations.length; i++){
+//             int h = 0;
+//             for(int j = 0; j < citations.length; j++){
+//                 if(citations[i] <= citations[j]){
+//                     h++;
+//                 }
+//                 if(h > citations[i]){
+//                     h = 0;
+//                 }
+//             }
+//             hIdxArr[i] = h;
+//         }
+
+//         Arrays.sort(hIdxArr);
+//         return hIdxArr[(citations.length)-1];
+
+//     }
+    
+    // 코드 2
+    public int solution(int[] citations) {
+        int answer = 0;
+        Arrays.sort(citations);
         for(int i = 0; i < citations.length; i++){
-            int h = 0;
-            for(int j = 0; j < citations.length; j++){
-                if(citations[i] <= citations[j]){
-                    h++;
-                }
-                if(h > citations[i]){
-                    h = 0;
-                }
+            int h = citations.length - i;
+            if(citations[i] >= h){
+                answer = h;
+                break;
             }
-            hIdxArr[i] = h;
         }
-
-        Arrays.sort(hIdxArr);
-        return hIdxArr[(citations.length)-1];
-
+        return answer;
     }
 }
