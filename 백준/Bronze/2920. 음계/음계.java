9af40bc1c355ae554e21c717ea2facc65041d73a
LEE YOUBIN <youbbqlsl@gmail.com>
2024-04-12 11:53:46 +0900

    [Bronze II] Title: 음계, Time: 124 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String st = br.readLine();
+        if(st.equals("1 2 3 4 5 6 7 8")) System.out.println("ascending");
+        else if(st.equals("8 7 6 5 4 3 2 1")) System.out.println("descending");
+        else System.out.println("mixed");
+    }
+}
