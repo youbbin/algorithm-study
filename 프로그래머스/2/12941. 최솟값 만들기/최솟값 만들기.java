df2daf1df675d18e961ce756d52151e6d6ac88a9
LEE YOUBIN <youbbqlsl@gmail.com>
2024-04-19 13:18:39 +0900

    [level 2] Title: 최솟값 만들기, Time: 1.89 ms, Memory: 62 MB -BaekjoonHub

---
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution
+{
+    public int solution(int[] A, int[] B) {
+        int sum = 0;
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        for(int i = 0; i < A.length; i++){
+            sum += A[i] * B[A.length-i-1];
+        }  
+        return sum;
+    }
+}
