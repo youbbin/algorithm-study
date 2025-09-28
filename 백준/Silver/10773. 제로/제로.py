5557d59295c0b62764c5762a76435fc68c3ab71a
Youbin Lee <youbbqlsl@gmail.com>
2025-09-28 21:50:30 +0900

    [Silver IV] Title: 제로, Time: 68 ms, Memory: 33192 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+K = int(input())
+stack = []
+
+for _ in range(K):
+    x = int(input())
+    if x == 0:
+        stack.pop()
+    else:
+        stack.append(x)
+
+print(sum(stack))
